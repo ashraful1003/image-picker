1d1a70c3d6274197424f07cf6bafa129e52f90ab
AshrafulAlam <ashraful.alam@brainstation-23.com>
2024-03-15 15:20:25 +0600

    ch

--- lib/screens/home/views/home_screen.dart
+++ lib/screens/home/views/home_screen.dart
@@ -47,7 +47,7 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Post Commercial Offer'),
+        title: const Text('Post Commercial Offer'),
       ),
       body: SingleChildScrollView(
           child: Column(
